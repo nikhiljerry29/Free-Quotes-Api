2e8e5e64f9ea935e9c2b2f341a4149ea8805c7b2
nikhiljerry29 <nikhil.gupta5667@gmail.com>
2021-09-02 09:38:23 +0530

    Add post route

--- routes/quotes.route.js
+++ routes/quotes.route.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const quoteController = require("../controllers/quote.controller");
 
 router.get("/quotes", quoteController.getQuotes);
-// router.post("/quotes", quoteController.postQuotes);
+router.post("/quotes", quoteController.postQuotes);
 
 router.get("/random", quoteController.getRandomQuote);
 
